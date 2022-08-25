c6104edfa92894921f7a0218b2888febde2e08f5
Calvin <ca7vin@gmail.com>
2022-08-25 11:30:07 +0200

    Python Conditions

---
+++ exercice_07_boolcondition/main.py
@@ -0,0 +1,47 @@
+def dmd_nom():
+    nom_response = ""
+    while nom_response == "":
+        nom_response = input("Quel est votre nom ?")    
+    return nom_response
+
+
+def dmd_age(nom_personne):
+    age_int = 0
+    while age_int == 0:
+        age_str = input(nom_personne + ", quel est votre age ?")
+        try:
+            age_int = int(age_str)
+        except ValueError:
+            print("ERROR: Vous devez rentrer un nombre pour l'age !")
+    return age_int
+
+
+def print_infos(nom_personne, age_personne):
+    print()
+    print("Vous vous appeler " + nom_personne + " et vous avez " + str(age_personne) + " ans")
+    print("L'an prochain vous aurez " + str(age_personne+1))
+
+    if age_personne > 18:
+        print("Vous êtes majeur !")
+    elif age_personne == 17:
+        print("Vous êtes bientôt majeur! ")
+    elif age_personne == 18:
+        print("Vous êtes tout juste majeur !")
+    elif age_personne <= 18 :
+        print("Vous êtes mineur !")
+
+
+#
+# ------------------------------------------------------------------------
+# 
+#demander le nom
+nom1 = dmd_nom()
+nom2 = dmd_nom()
+
+#demander l'age
+age1 = dmd_age(nom1)
+age2 = dmd_age(nom2)
+
+# afficher
+print_infos(nom1, age1)
+print_infos(nom2, age2)
